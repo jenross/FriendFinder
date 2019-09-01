1ce0f8ce3dc80c4f883fda8799ad27045b0e9e25
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-09-01 15:18:40 -0400

    updated friends and survey questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,44 +1,66 @@
 const friends = [
     {
-       "name": "Jen", 
-       "photo": "",
+       "name": "Eleanor Shellstrop", 
+       "photo": "https://media.giphy.com/media/3ohs7QsBjK2EkvlLuU/giphy.gif",
        "scores":[
-
+            5, 
+            2, 
+            4, 
+            3, 
+            1, 
+            5, 
+            1, 
+            4, 
+            1, 
+            1
        ]
     },
     {
-        "name": "Jen", 
-        "photo": "",
-        "scores":[
-            
-        ]
+      "name": "Jason Mendoza", 
+      "photo": "https://media.giphy.com/media/3ohs80ubv3iyojPquY/giphy.gif",
+      "scores":[
+           4, 
+           5, 
+           3, 
+           5, 
+           1, 
+           1, 
+           1, 
+           4, 
+           1, 
+           1
+      ]
      }, 
      {
-        "name": "Jen", 
-        "photo": "",
-        "scores":[
-            
-        ]
+      "name": "Michael", 
+      "photo": "https://media.giphy.com/media/xT0xes9FP7JVi8LxoA/giphy.gif",
+      "scores":[
+           1, 
+           1, 
+           5, 
+           3, 
+           4, 
+           1, 
+           1, 
+           4, 
+           2, 
+           5
+      ]
      }, 
      {
-        "name": "Jen", 
-        "photo": "",
-        "scores":[
-            
-        ]
+      "name": "Tahani Al Jamil", 
+      "photo": "https://media.giphy.com/media/3o6fJ2njgvaC49hQli/giphy.gif",
+      "scores":[
+           1, 
+           1, 
+           4, 
+           1, 
+           5, 
+           1, 
+           1, 
+           5, 
+           3, 
+           3
+      ]
      }, 
-     {
-        "name": "Jen", 
-        "photo": "",
-        "scores":[
-            
-        ]
-     }, 
-     {
-        "name": "Jen", 
-        "photo": "",
-        "scores":[
-            
-        ]
-     }
 ];
