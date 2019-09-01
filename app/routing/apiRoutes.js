597cb3ef0db596d53e37530fca29f6eb2b6ae1b0
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-09-01 12:53:53 -0400

    added apiroutes get and post

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,49 @@
+// ===============================================================================
+// LOAD DATA
+
+const friendData = require("../data/friends");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+
+  // API POST Requests
+
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    // if (tableData.length < 5) {
+    //   tableData.push(req.body);
+    //   res.json(true);
+    // }
+    // else {
+    //   waitListData.push(req.body);
+    //   res.json(false);
+    // }
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    friendData.length = 0;
+
+    res.json({ ok: true });
+  });
+};
