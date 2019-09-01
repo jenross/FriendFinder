9041b2a71d0179b6d50f7444c4390169a28cbf65
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-09-01 12:47:45 -0400

    updated htmlroutes and main server with required dependencies and get requests

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 // Dependencies
 // =============================================================
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
